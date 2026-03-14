2c3115183249511533d45fab1c6313276b8f6d26
lorenahyseni-cyber <lorenahyseni1010@gmail.com>
2026-03-14 12:05:02 +0100

    Initail commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function Counter() {
+    // state number
+    const [count, setCount] = useState(0);
+    // state string
+    const [name, setName] = useState("");
+    // state boolean
+    const [show, setShow] = useState(true);
+
+    const [users, setUsers] = useState([
+        { id: 1, firstName: "egzon", role: "instructor" },
+        { id: 2, firstName: "leon", role: "instructors" },
+        { id: 3, firstName: "leotrim", role: "instructorss" },
+    ])
+
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>
+                increase
+            </button>
+            <h1>{count}</h1>
+            <div>
+                <input type="text"
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <h1>{name}</h1>
+            </div>
+            <div>
+                {show && <p>this is hidden</p>}
+            </div>
+            <div>
+                <div>
+                    {users.map(user => (
+                        <p key={user.id}>{user.firstName} {user.role }</p>
+                    ))}
+                </div>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Counter;
